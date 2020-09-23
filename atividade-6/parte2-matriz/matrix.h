bfabe2525545ab1efc95796fb5cafa5ae6303190
kennethufmg <42329236+kennethufmg@users.noreply.github.com>
2020-09-23 12:49:26 -0300

    Update matrix.h

--- atividade-6/parte2-matriz/matrix.h
+++ atividade-6/parte2-matriz/matrix.h
@@ -1,31 +1,60 @@
-// matrix.h
-
-#ifndef MATRIX_H_
-#define MATRIX_H_
-
+#ifndef MATRIX_H
+#define MATRIX_H
+// matrix.h (header file)
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-template <class TValor> class Matrix;
+template<class T> 
+class Matrix
+{
+private:
+	T** m; // m é um array 2D a ser implementado como um pointer de pointers
+	int nRows;  // numero de linhas
+	int nCols;  //  numero de colunas
 
-template <class TValor> 
-ostream& operator<<(ostream&, const Matrix<TValor>&);
+public:
 
-template <class TValor>
-class Matrix {
-    
-    private:
-        ... 
-        
-    public:
-        //Construtores 
-        Matrix();
-        Matrix(int linhas, int colunas);
-        Matrix(int linhas, int colunas, const TValor& valor);
-        Matrix(const Matrix &matriz);
-        // Destrutor
-        ~Matrix();
-        
-        TValor& operator()(const int, const int) const; 
-        ...
+	// Construtores
+	Matrix();
+	Matrix(int rows, int cols,const T& value=0);
+	Matrix(std::ifstream &myFile);
+	Matrix(const Matrix &that);
+	//
+	// destrutor
+	~Matrix();
+
+	// basic getters
+	int getRows() const;
+	int getCols() const;
+	T get(int ls, int cs) const;
+	//Funções
+	void transpose();
+	void print() const;
+	void unit();
+	void zeros();
+	void ones();
+	//Sobrecarga de operando
+	Matrix operator + (const Matrix &Right);
+	Matrix operator - (const Matrix &Right);
+	Matrix& operator =(const Matrix &Right);
+	bool operator == (const Matrix &Right);
+	bool operator != (const Matrix &Right);
+	T& operator() (const int &ls, const int &cs) const;
+	Matrix operator * (const Matrix &Right);
+	Matrix operator ~();
+	Matrix& operator -= (const Matrix &Right);
+	Matrix& operator += (const Matrix &Right);
+	Matrix& operator *= (const double n);
+	Matrix& operator *= (const Matrix &Right);
+	/* OPERACOES  << >>  */
+	/* OPERACOES  << >>  */
+
+	friend std::istream& operator >> (std::istream& text, Matrix& Right);
+	friend std::ostream& operator << (std::ostream& text, const Matrix& Right);
+
 
 };
+
+#endif //MATRIX_H
