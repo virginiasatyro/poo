e7133f9ebd378315aae478fd28f8171e646b2bbd
kennethufmg <42329236+kennethufmg@users.noreply.github.com>
2020-10-26 14:04:26 -0300

    Update Banco.cpp

--- atividade-10/Banco.cpp
+++ atividade-10/Banco.cpp
@@ -45,27 +45,28 @@ int main()
 	}
 
 // TESTE DE IMPRESSAO DA TAXA DE RENDIMENTO VIA RTTI
-	cout << "::Taxa de rendimento para cada conta::" << endl;
+	cout << "::Taxa de rendimento para cada conta POUPANCA::" << endl;
 	for (int i = 0; i < (signed)ContasBanco.size(); i++)
 	{
 		ContaPoupanca *OK = dynamic_cast<ContaPoupanca*>(ContasBanco[i]);
 		if (OK)
 		{
-			cout << "Conta Poupanca numero: " << ContasBanco[i]->get_numeroConta();
-			cout << "Taxa:"<<ContasBanco[i]->DadoUnico() << endl;
+			cout << "::Conta Poupanca numero: " << ContasBanco[i]->get_numeroConta();
+			cout << "::Taxa:"<<ContasBanco[i]->DadoUnico() << endl;
 		}
-
 	}
 
 // REALIZANDO DEPOSITOS
-double deposito;
 //DEPOSITANDO UM VALOR ALEATORIO EM CADA CONTA
+	cout << ":: REALIZANDO DEPOSITOS DE VALORES ALEATORIOS NAS CONTAS ::" << endl;
+double deposito;
 for (int i = 0; i < (signed)ContasBanco.size(); i++)
 {
 	deposito = rand() % 600;
 	ContasBanco[i]->deposita(deposito);
 }
 //DEPOSITANDO UM VALOR DE 250 REAIS SOMENTE NAS CONTAS CORRENTE
+cout << ":: REALIZANDO DEPOSITOS DE R$ 250 NAS CONTAS CORRENTE ::" << endl;
 
 for (int i = 0; i < (signed)ContasBanco.size(); i++)
 {
@@ -75,9 +76,9 @@ for (int i = 0; i < (signed)ContasBanco.size(); i++)
 		ContasBanco[i]->deposita(250);
 	}
 }
-
+cout <<" "<< endl;
 //DEPOSITANDO UM VALOR DE 1000 REAIS SOMENTE NAS CONTAS POUPANCA
-
+cout << ":: REALIZANDO DEPOSITOS DE R$ 1000 NAS CONTAS POUPANCA::" << endl;
 for (int i = 0; i < (signed)ContasBanco.size(); i++)
 {
 	ContaPoupanca *OK = dynamic_cast<ContaPoupanca*>(ContasBanco[i]);
@@ -86,49 +87,52 @@ for (int i = 0; i < (signed)ContasBanco.size(); i++)
 		ContasBanco[i]->deposita(1000);
 	}
 }
-
+cout << " " << endl;
+cout << ":: REALIZANDO SAQUES ALEATORIOS EM CADA CONTA CRIADA::" << endl;
 // REALIZANDO SAQUES
 // VALOR DE SAQUES ALEATORIOS
 double saque;
 for (int i = 0; i < (signed)ContasBanco.size(); i++)
 {
 	saque = rand() % 1000;
+	cout << ":: SAQUE de R$ " << saque << " na conta numero: " << ContasBanco[i]->get_numeroConta() << " :: " << endl;
+	cout << ":: Saldo disponivel :R$ "<< ContasBanco[i]->get_Saldo() << " :: " << endl;
 	//PODEM OCORRER VALORES DE SAQUE MAIORES DO QUE O VALOR  DISPONIVEL NA CONTA, PARA ISSO USAMOS O TRY ANDA CATCH
-	
-	// Implementar Try and catch
-	// Vou dormir 
-	
-	
-	
-	
-	
-	
-	ContasBanco[i]->saca(saque);
-}
+	try {
+		ContasBanco[i]->saca(saque);
+	}
+	catch (Erro& ex)
+	{
+		ex.out();
+		cout << endl;
+	}
 
+}
+cout << " " << endl;
 // TAXA DE RENDIMENTO DAS CONTAS CORRENTES VIA RTTI
 for (int i = 0; i < (signed)ContasBanco.size(); i++)
 	{
 		ContaPoupanca *OK = dynamic_cast<ContaPoupanca*>(ContasBanco[i]);
 		if (OK)
 		{
-			cout << "Conta Poupanca numero: " << ContasBanco[i]->get_numeroConta();
-			cout << "Taxa:" << ContasBanco[i]->DadoUnico() << endl;
+			cout << "::Conta Poupanca numero: " << ContasBanco[i]->get_numeroConta();
+			cout << "::Taxa:" << ContasBanco[i]->DadoUnico() << endl;
 		}
 	}
 
 // NUMERO DE TRANSACOES DAS CONTAS CORRENTES VI RTTI
-
+cout << " " << endl;
 for (int i = 0; i < (signed)ContasBanco.size(); i++)
 {
 	ContaCorrente *OK = dynamic_cast<ContaCorrente*>(ContasBanco[i]);
 	if (OK)
 	{
-		cout << "Conta Corrente numero: " << ContasBanco[i]->get_numeroConta();
-		cout << "Numero de transacoes:" << ContasBanco[i]->DadoUnico() << endl;
+		cout << "::Conta Corrente numero: " << ContasBanco[i]->get_numeroConta();
+		cout << "::Numero de transacoes:" << ContasBanco[i]->DadoUnico() << endl;
 	}
 }
-
+cout << " " << endl;
+cout << ":: IMPRIMINDO EXTRATOS ::" << endl;
 // IMPRESSAO DO EXTRATO - CRIAMOS POR NOSSA INTERPRETACAO E PARA GANHAR PONTOS EXTRA
 // A CLASSE EXTRATO QUE ARMAZENA O TIPO DA OPERACAO FINANCEIRA, VALOR SACADO OU RETIRADO, E O VALOR DE DINHEIRO NA CONTA
 // QUE SE APROXIMA DO EXTRATO QUE RETIRAMOS NUM BANCO
@@ -137,10 +141,11 @@ for (int i = 0; i < (signed)ContasBanco.size(); i++)
 {
 	ContasBanco[i]->tiraExtrato();
 }
+cout << " " << endl;
 
 // O banco fechou, vamos deletar tudo, apagando contas e dinheiro também
 // DELETANDO MEMORIA ALOCADA
-for (int i = 0; i < (signed) ContasBanco.size() - 1; i++)
+for (int i = 0; i < (signed) ContasBanco.size(); i++)
 {
 	delete ContasBanco[i];
 }
